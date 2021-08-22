362a44015e40b42e2ad1b7ee0fb8235c5a42d504
KaMeHb-UA <marlock@bassteam.ml>
2021-08-23 02:01:19 +0300

    add filters option

--- app/components/db_get.js
+++ app/components/db_get.js
@@ -1,8 +1,17 @@
 import get from '../helpers/db.js';
 
+const ops = {
+    '=': (op1, op2) => op1 === op2,
+    '!=': (op1, op2) => op1 !== op2,
+};
+
 export default ({ table, options }) => {
-    const { limit, reverse } = options || {};
-    let tbl = get(table);
+    const { limit, reverse, filters } = options || {};
+    let tbl = get(table).map((data, id) => ({ id: id + '', data }));
+    if(filters) for(const [name, op, val] of filters){
+        const cb = ops[op];
+        tbl = tbl.filter(({ data }) => cb(data[name], val));
+    }
     if(reverse) tbl = tbl.reverse();
     if(limit) tbl = tbl.slice(0, limit);
     return tbl;
